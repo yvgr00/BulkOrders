855395a6e686e8d2044f4191dbab252aa72cf140
venu gopal reddy <yvgr00@gmail.com>
2021-03-02 20:46:03 -0500

    linked orderservice to kafka consumer

--- src/main/java/com/orderprocessing/orders/consumers/Consumer.java
+++ src/main/java/com/orderprocessing/orders/consumers/Consumer.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import com.orderprocessing.orders.dto.RequestDTO;
+import com.orderprocessing.orders.services.OrderService;
 
 @Service
 public class Consumer {
@@ -17,13 +19,17 @@ public class Consumer {
 	private final Logger logger = LoggerFactory.getLogger(Consumer.class);
 	
 //	private OrderController ordCont;
+	
+	@Autowired
+	private OrderService orderService;
+
 
     @KafkaListener(id = "batch", topics = "${spring.kafka.topic.app}"	)
     public void consume(@Payload RequestDTO messages) throws IOException {
     	
+    	orderService.saveOrder(messages);
     	
-    	
-            logger.info("Received message='{}' with partition-offset='{}'", messages.getShipping_address_line1());
+        logger.info("Received message='{}' with partition-offset='{}'", messages.getShipping_address_line1());
         
         logger.info("All batch messages received");
        
